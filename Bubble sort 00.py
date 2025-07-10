6b8878db036cca1eef698ee29a5feb3d1c6fdc30
srijasaithya01 <91382163+srijasaithya01@users.noreply.github.com>
2025-07-10 17:59:17 +0530

    Update Bubble sort 00.py

--- Bubble sort 00.py
+++ Bubble sort 00.py
@@ -1,8 +1,8 @@
 #Bubble Sort
-def bubble_sort(arr, n): #defining
+def bubble_sort(arr, n): #defining function
     for i in range(n-1):    #Outer loop   i=> passes
         for j in range(n-i-1):   #Inner loop  j=> position
-             if a[j] > a[j+1]:
+             if a[j] > a[j+1]:    #if loop
                  a[j], a[j+1] = a[j+1], a[j]
 a = [2, 56, 45, 13, 9, 1] #array
 n = len(a) #length => no. of elements
