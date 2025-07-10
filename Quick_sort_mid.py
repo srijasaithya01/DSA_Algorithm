ef8dd69612b1fdd1d41a8fe34cb496a05457b4e1
srijasaithya01 <91382163+srijasaithya01@users.noreply.github.com>
2025-07-11 01:54:08 +0530

    Update Quick_sort_mid.py

--- Quick_sort_mid.py
+++ Quick_sort_mid.py
@@ -1,22 +1,23 @@
-def Quick_sort(arr, left, right):
-    i = left
+def Quick_sort(arr, left, right): #defining the function
+    i = left      
     j = right
     mid = int((left+right)/2)
-    piv = arr[mid]
-    while i<=j:
-        while arr[i]<piv:
-            i+=1
+    piv = arr[mid]  
+    #Nesting loop
+    while i<=j:    #outer loop, while loop
+        while arr[i]<piv:   #inner loop
+            i+=1      #increment
         while arr[j]>piv:
-            j-=1
+            j-=1      #decrement
         if i<=j:
             arr[i], arr[j] =arr[j],arr[i]
             i+=1
             j-=1
-    if i<right:
+    if i<right: 
         Quick_sort(arr,i,right)
     if j>left:
         Quick_sort(arr,left,j)
-a = [45,32,57,12,54,77]
-n = len(a)
-Quick_sort(a,0,n-1)
-print(a)
+a = [45,32,57,12,54,77] 
+n = len(a)   #number of elements
+Quick_sort(a,0,n-1)   #calling function
+print(a)  #printing
